1c88e47f879bee1ad090c2566470063bcddb993c
Rohit Gupta <grohit7711@gmail.com>
2021-08-17 13:23:37 +0530

    add isDev

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+const isDev = !app.isPackaged
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 1400,
@@ -18,20 +20,22 @@ function createWindow() {
     win.loadFile('index.html')
 }
 
-require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-})
+if (isDev) {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    })
+}
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin'){
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+        createWindow();
     }
 })
